6356f36ac8acf81e56dd6aa3d2b16dd93d608ab6
daniel692a <daniel62armas@gmail.com>
2021-11-28 19:41:41 -0600

    update Dockerfile to Heroku deploy

--- compose/app.py
+++ compose/app.py
@@ -1,22 +1,12 @@
-import time
-import redis
 from flask import Flask
+import os
 
 app = Flask(__name__)
-cache = redis.Redis(host='redis', port=6379)
-
-def get_hit_count():
-    retries = 5
-    while True:
-        try:
-            return cache.incr('hits')
-        except redis.exceptions.ConnectionError as exc:
-            if retries == 0:
-                raise exc
-            retries -= 1
-            time.sleep(0.5)
 
 @app.route('/')
 def hello():
-    count =  get_hit_count()
-    return f"<h1>My first docker container</h1><br>Hello world! I've seen {count} times."
+    return "Hello World!"
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host = '0.0.0.0', port = port)
